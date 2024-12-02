983cb8d98d2073f3f08a2b388ce9e7aeb30603ed
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2024-12-02 01:54:14 +0100

    feat(BE): add cargo booking types

---
+++ backend/src/main/kotlin/com/mrizki/cargomanagement/booking/model/CargoType.kt
@@ -0,0 +1,12 @@
+package com.mrizki.cargomanagement.booking.model
+
+enum class CargoType {
+    GENERAL,
+    SPECIAL,
+    LIVE_ANIMALS,
+    HAZARDOUS,
+    HIGH_VALUE,
+    FRAGILE,
+    PERISHABLE,
+    MAIL
+}
