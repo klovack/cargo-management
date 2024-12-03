9d4d750a7ca3d6a67758d0e7b93092c1b8cd4951
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2024-12-03 01:48:45 +0100

    feat(BE): add get all bookings endpoint

--- backend/src/main/kotlin/com/mrizki/cargomanagement/booking/CargoBookingController.kt
+++ backend/src/main/kotlin/com/mrizki/cargomanagement/booking/CargoBookingController.kt
@@ -1,15 +1,17 @@
 package com.mrizki.cargomanagement.booking
 
 import com.mrizki.cargomanagement.booking.model.*
+import com.mrizki.cargomanagement.util.ValidSortBy
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import jakarta.validation.constraints.Pattern
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/bookings")
@@ -24,5 +26,47 @@ class CargoBookingController(
             cargoBookingService.createBooking(cargoBooking),
             HttpStatus.CREATED,
         )
+    }
+
+    @GetMapping
+    fun getAllBookings(
+        @RequestParam(required = false, defaultValue = "0") page: Int,
+        @RequestParam(required = false, defaultValue = "20") pageSize: Int,
+        @RequestParam(required = false)
+        @ValidSortBy(
+            target = CargoBookingDTO::class,
+            message = "invalid sort by for CargoBookingDTO"
+        )
+        sortBy: String?,
+        @RequestParam(required = false)
+        @Pattern(regexp = "ASC|DESC", message = "sortDirection must be ASC or DESC")
+        sortDirection: String?,
+        @RequestParam(required = false) name: String?,
+        @RequestParam(required = false) destination: String?,
+    ): ResponseEntity<Page<CargoBookingDTO>> {
+        var pageRequest = PageRequest.of(page, pageSize)
+
+        if (sortBy != null) {
+            val sort = Sort.by(direction(sortDirection), sortBy)
+            pageRequest = pageRequest.withSort(sort)
+
+        }
+
+        return ResponseEntity(
+            cargoBookingService.getAllBookings(pageRequest, name, destination),
+            HttpStatus.OK
+        )
+    }
+
+    private fun direction(sortDirection: String?): Sort.Direction {
+        return if (sortDirection != null) {
+            try {
+                Sort.Direction.valueOf(sortDirection)
+            } catch (e: IllegalArgumentException) {
+                Sort.Direction.ASC
+            }
+        } else {
+            Sort.Direction.ASC
+        }
     }
 }
--- backend/src/main/kotlin/com/mrizki/cargomanagement/booking/CargoBookingServiceMongoDB.kt
+++ backend/src/main/kotlin/com/mrizki/cargomanagement/booking/CargoBookingServiceMongoDB.kt
@@ -4,13 +4,38 @@ import com.mrizki.cargomanagement.booking.model.CargoBookingDTO
 import com.mrizki.cargomanagement.booking.model.CargoBookingInput
 import com.mrizki.cargomanagement.booking.model.toDTO
 import com.mrizki.cargomanagement.booking.model.toEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class CargoBookingServiceMongoDB(
     val cargoBookingRepository: CargoBookingRepositoryMongoDB
-): CargoBookingService {
+) : CargoBookingService {
     override fun createBooking(cargoBookingInput: CargoBookingInput): CargoBookingDTO {
         return toDTO(cargoBookingRepository.insert(toEntity(cargoBookingInput)))
+    }
+
+    override fun getAllBookings(
+        pageable: Pageable,
+        name: String?,
+        destination: String?
+    ): Page<CargoBookingDTO> {
+        val cargoBookings = if (name != null && destination != null) {
+            cargoBookingRepository
+                .findAllByNameIsContainingIgnoreCaseAndDestinationContainingIgnoreCase(
+                    name,
+                    destination,
+                    pageable
+                )
+        } else if (destination != null) {
+            cargoBookingRepository.findAllByDestinationContainingIgnoreCase(destination, pageable)
+        } else if (name != null) {
+            cargoBookingRepository.findAllByNameContainingIgnoreCase(name, pageable)
+        } else {
+            cargoBookingRepository.findAll(pageable)
+        }
+
+        return cargoBookings.map { toDTO(it) }
     }
 }
